be5b20f61ba1d7ac2726c545c8d847dbd7ebfea3
LuizNach <11381501+LuizNach@users.noreply.github.com>
2025-06-08 17:40:13 -0300

    chore: Add database session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,26 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from fastapi_zero.app import app
+from fastapi_zero.models import table_registry
 
 
 @pytest.fixture
 def client() -> TestClient:
     # Arrange
     return TestClient(app=app)
+
+
+@pytest.fixture
+def session():
+    # Setup/Arrange
+    engine = create_engine(url='sqlite:///:memory')
+    table_registry.metadata.create_all(bind=engine)
+
+    with Session(engine) as session:
+        yield session
+
+    # Teardown
+    table_registry.metadata.drop_all(bind=engine)
